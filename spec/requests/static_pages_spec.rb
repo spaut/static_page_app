2366362440c99bc30699264e59f03b41f13a0f8a
Ron Pritchard <rpritch@gmail.com>
2013-06-21 07:20:01 -0700

    finished chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,10 +8,16 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
     it "should have the right title" do
   		visit '/static_pages/home'
-  		page.should have_selector('title',
-                    	:text => "Ruby on Rails Tutorial Sample App | Home")
+  		page.should_not have_selector('title',
+                    	:text => "| Home")
+
 	end
   end
 
