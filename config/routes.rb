2ccb32e91e34bcf40687faeb78532cbe94bceb3e
Ron Pritchard <rpritch@gmail.com>
2013-06-23 11:24:22 -0700

    up to 8.2 - valid sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 StaticPageApp::Application.routes.draw do
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  
+  
+  root to: 'static_pages#home'
+  
+  # match '/', to: 'static_pages#home'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
   # get "static_pages/home"
 
   # get "static_pages/help"
@@ -7,14 +22,6 @@ StaticPageApp::Application.routes.draw do
   # get "static_pages/about"
 
   # get "static_pages/contact"
-
-  root to: 'static_pages#home'
-  
-  # match '/', to: 'static_pages#home'
-  match '/signup',  to: 'users#new'
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
